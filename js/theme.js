0204cc76db593dec13160a2fbfbe627b87516153
Yuxi Luo <luoyx2014@163.com>
2017-08-04 01:07:33 -0400

    Update project page

---
+++ js/theme.js
@@ -0,0 +1,17 @@
+/**
+ * Created by luoyuxi on 2017/8/3.
+ */
+
+$(document).ready(function () {
+    Nav.initiate();
+});
+
+var Nav = {
+    initiate: function(){
+
+    },
+    initNav: function(){
+
+    }
+
+}
