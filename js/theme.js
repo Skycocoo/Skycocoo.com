5345e1a6ebd00a731e0456fb4ca6dcd4af0a7cde
Yuxi Luo <luoyx2014@163.com>
2017-08-05 23:31:17 -0400

    Attempt to be responsive in navigator bar

--- js/theme.js
+++ js/theme.js
@@ -1,17 +1,31 @@
 /**
  * Created by luoyuxi on 2017/8/3.
+ * Navigator inspired by https://liby.me/index.html
  */
 
 $(document).ready(function () {
-    Nav.initiate();
+    Nav.initNav();
 });
 
 var Nav = {
-    initiate: function(){
-
-    },
+    $menu: $("#navi-menu"),
+    $square: $("#navi-logo"),
     initNav: function(){
-
+        this.$square.click(function(){
+            if(this.menuon()) {
+                this.hidem();
+            } else {
+                this.showm();
+            }
+        });
+    },
+    menuon: function(){
+        return this.$menu.hasClass("active");
+    },
+    hidem: function () {
+        this.$menu.removeClass("active");
+    },
+    showm: function () {
+        this.$menu.addClass("active");
     }
-
-}
+};
