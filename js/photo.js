5255babb1bacb9df31327b679941532f11419393
Skycocoo <luoyx2014@163.com>
2018-01-27 11:19:48 -0500

    New photo page & footer style

---
+++ js/photo.js
@@ -0,0 +1,31 @@
+var listp = [{
+    name: "trees",
+    amount: 16,
+    cur: 0
+}, {
+    name: "manhattan",
+    amount: 8,
+    cur: 0
+}, {
+    name: "bklyn",
+    amount: 2,
+    cur: 0
+}];
+
+function demos () {
+    for (var i = 0; i < listp.length; i++) {
+        for (var j = 0; j < listp[i].amount; j++) {
+            var dir = "figures/" + listp[i].name + "/" + (j + 1) + ".jpg";
+            var elem = "<div class=\"image\" style=\"background-image: url('" + dir + "');\" />";
+
+            // append images to id
+            var photo = $(elem).appendTo("#" + listp[i].name);
+        }
+    }
+}
+
+
+$(document).ready(function(){
+    demos();
+
+});
