e8882e00924f81e435f19bfd84577a79ad795d33
Skycocoo <luoyx2014@163.com>
2018-05-28 15:33:13 -0400

    Finish light loading

--- test/loading.js
+++ test/loading.js
@@ -7,24 +7,72 @@ function fade() {
     );
 }
 
-var time = 0;
+var time = 0, time2 = 0;
 
 function load4() {
     var id = setInterval(
         function(){
             // stop the loop when time exceed 3s
-            if (time >= 28.3) clearInterval(id);
+            if (time >= 14.15) clearInterval(id);
 
-            time += 0.1;
-            var sine = Math.sin(time);
-            var cosine = Math.cos(time);
+            time += 0.05;
+            var sine = Math.sin(2*time);
+            var cosine = Math.cos(2*time);
+
+            var randsin = Math.random() * sine;
+            var randcos = Math.random() * cosine;
 
             // function reference:
             // https://math.stackexchange.com/a/7991
-            $("#load4").css({"top": 40 * sine * cosine,
-                             "left": 40 * cosine,
-                             "background-color": "rgb(" + (20 + 20 * sine) + ", " + (181 + 50 * sine) + ", " + (204 + 50 * cosine) + ")"
+            $("#load4").css({"top": 70 * sine * cosine,
+                             "left": 70 * cosine,
+                             "background-color": "rgb(" + (20 + 20 * randsin) + ", " + (181 + 50 * randsin) + ", " + (204 + 50 * randcos) + ")"
                             });
+
+        }, 10
+    );
+};
+
+
+function load5() {
+    var id = setInterval(
+        function(){
+            // stop the loop when time exceed 3s
+            if (time2 >= 14.15) clearInterval(id);
+
+            time2 += 0.05;
+
+            var sine = Math.sin(time2);
+            var cosine = Math.cos(time2);
+
+            var y = 6 * cosine;
+            var x = 2 * sine * cosine;
+            var randsin = Math.random() * sine;
+            var randcos = Math.random() * cosine;
+
+            // if (y < 0) y = 0;
+
+            // radius change by the displacement of y
+            var radius = 15 + y;
+
+            // function reference:
+            // https://math.stackexchange.com/a/7991
+            $("#load5").css({
+                "top": y,
+                "left": x,
+                "background-color": "rgb(" + (235 + 20 * randcos) + ", " + (245 + 10 * randsin) + ", " + (50 + 50 * randcos) + ")",
+                "width": radius,
+                "height": radius,
+                "border-radius": radius
+            });
+
+            // map [0, 8] to [0, f]
+            // #000 to #fff
+            var conv = Math.floor(y / 8 * 15).toString(16);
+
+            $("#load5-bg").css({
+                "background-color": "#" + conv + conv + conv
+            });
 
         }, 10
     );
@@ -34,4 +82,5 @@ function load4() {
 $(document).ready(function(){
     // fade();
     load4();
+    load5();
 });
