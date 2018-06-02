0a91ae351fdc5677ae393c618d24532bf700d0be
Skycocoo <luoyx2014@163.com>
2018-06-02 11:00:01 -0400

    Need to calc loading time for img

--- js/photo.js
+++ js/photo.js
@@ -1,14 +1,3 @@
-// http://www.adequatelygood.com/JavaScript-Scoping-and-Hoisting.html
-
-
-var loadTime = 0;
-window.onload = function () {
-    loadTime = window.performance.timing.domContentLoadedEventEnd-window.performance.timing.navigationStart;
-    console.log('Page load time is '+ loadTime);
-}
-
-
-
 var listp = [{
     name: "trees",
     amount: 16,
@@ -23,6 +12,7 @@ var listp = [{
     cur: 0
 }];
 
+
 function demos () {
     for (var i = 0; i < listp.length; i++) {
         for (var j = 0; j < listp[i].amount; j++) {
@@ -30,34 +20,20 @@ function demos () {
             var elem = "<div class=\"image\" style=\"background-image: url('" + dir + "');\" />";
             // append images to id
             var photo = $(elem).appendTo("#" + listp[i].name);
-
-            var startTime = new Date().getTime();
-            var loadtime = 0;
-            var img = new Image();
-            img.onload = function() {
-                var loadtime = new Date().getTime() - startTime;
-                console.log(typeof(loadtime) + loadtime);
-            };
-            img.src = dir;
-
-            loadTime += loadtime;
-
         }
     }
 }
 
-function fade() {
+function fade(loading) {
     setTimeout(
         function(){
             $(".loader").addClass("loaded");
-            console.log("spanning time:", loadTime);
         },
-        loadTime
+        loading
     );
 }
 
 $(document).ready(function(){
     demos();
-    console.log("after demos:" + loadTime);
-    fade();
+    fade(1500);
 });
