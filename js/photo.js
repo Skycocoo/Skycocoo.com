352730c5a9686d2a481d460947d2e99f35e7ffcc
Skycocoo <luoyx2014@163.com>
2018-06-03 15:48:19 -0400

    Update preloader & photolist

--- js/photo.js
+++ js/photo.js
@@ -1,15 +1,27 @@
+function fade(loading) {
+    setTimeout(
+        function(){
+            $(".loader").addClass("loaded");
+        },
+        loading
+    );
+}
+
 var listp = [{
     name: "trees",
-    amount: 4,
-    // amount: 16,
+    amount: 16,
+    title: "Trees NYC",
+    tag: "#Iphone #Seasons #Trees",
 }, {
     name: "manhattan",
-    amount: 1,
-    // amount: 8,
+    amount: 8,
+    title: "Manhattan NYC",
+    tag: "#Skline #Silhouette",
 }, {
     name: "bklyn",
-    amount: 1,
-    // amount: 2,
+    amount: 2,
+    title: "Brooklyn NYC",
+    tag: "#Sunrise #Rainbow",
 }];
 
 var current = 0;
@@ -17,6 +29,13 @@ var window_height = $(window).height();
 
 function demo() {
     if (current < listp.length) {
+        var descrip = "<descrip>" +
+                      "<title>" + listp[current].title + "</title>" +
+                      "<tag>" + listp[current].tag + "</tag>" +
+                      "</descrip>";
+
+        $(descrip).appendTo("#" + listp[current].name);
+
         for (var j = 0; j < listp[current].amount; j++) {
             var dir = "figures/" + listp[current].name + "/" + (j + 1) + ".jpg";
             var elem = "<div class=\"image\" style=\"background-image: url('" + dir + "');\" />";
@@ -28,7 +47,6 @@ function demo() {
 }
 
 $(window).scroll(function () {
-    // console.log("scrolltop: " + $(window).scrollTop() + " document: " + $(document).height() + " entire window: " + window_height);
     if ($(window).scrollTop() == $(document).height() - window_height) {
         demo();
     }
@@ -39,60 +57,11 @@ function load() {
 
     while ((current < listp.length) && ((offset + $("#photo-wrapper").height()) < window_height)) {
         demo();
-        console.log("distance: " + (offset + $("#photo-wrapper").height()));
     }
-    // var offset = $("content").offset().top + $("#content-header").height() - 3 * $("descrip").height();
-    //
-    // while ((offset + $("content").height()) < window_height) {
-    //     demo();
-    //     // console.log("distance: " + (offset + $("content").height()));
-    // }
 }
 
-
-// function demos () {
-//     for (var i = 0; i < listp.length; i++) {
-//         for (var j = 0; j < listp[i].amount; j++) {
-//             var dir = "figures/" + listp[i].name + "/" + (j + 1) + ".jpg";
-//             var elem = "<div class=\"image\" style=\"background-image: url('" + dir + "');\" />";
-//             // append images to id
-//             var photo = $(elem).appendTo("#" + listp[i].name);
-//         }
-//     }
-// }
-
-
-// var counter=0;
-// $(window).scroll(function () {
-//     if ($(window).scrollTop() == $(document).height() - $(window).height() && counter < 2) {
-//         appendData();
-//     }
-// });
-// function appendData() {
-//     var html = '';
-//     for (i = 0; i < 10; i++) {
-//         html += '<p class="dynamic">Dynamic Data :  This is test data.</br>Next line.</p>';
-//     }
-//     $('#myScroll').append(html);
-//     counter++;
-//
-//     if(counter==2)
-//     $('#myScroll').append('<button id="uniqueButton" style="margin-left: 50%; background-color: powderblue;">Click</button></br></br>');
-// }
-//
-//
-
-function fade(loading) {
-    setTimeout(
-        function(){
-            $(".loader").addClass("loaded");
-        },
-        loading
-    );
-}
 
 $(document).ready(function(){
     load();
-    // demo(current);
-    // fade(2500);
+    fade(1500);
 });
