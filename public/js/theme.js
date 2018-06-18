39eac05640346b8d3556dd041a6fc6676a2b8c4d
Skycocoo <luoyx2014@163.com>
2018-06-18 00:35:43 -0400

    Initialize index page; need to update new style

---
+++ public/js/theme.js
@@ -0,0 +1,24 @@
+// Created by Yuxi Luo; 2017
+
+var Nav = {
+    $navi: $("#navi"),
+    $menu: $("#navi-menu-mobi-out"),
+    $square: $("#navi-menu-mobi"),
+
+    initNav: function(){
+        var self = this;
+        if(self.$navi.width() < 750){
+            // self.$square.addClass("animated rubberBand");
+            self.$square.addClass("active");
+        }
+        self.$square.click(function(){
+            self.$menu.toggleClass("active");
+            self.$square.toggleClass("rot");
+        });
+    },
+};
+
+
+$(document).ready(function () {
+    Nav.initNav();
+});
