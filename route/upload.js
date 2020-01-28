2d58329b7e76a536f9b664cb4cd5160482c5a5e0
Skycocoo <luoyx2014@163.com>
2020-01-28 14:23:33 -0500

    Remove unused file

--- route/upload.js
+++ route/upload.js
@@ -1,29 +1,29 @@
-// Created by Yuxi Luo, July 2018
-
-const express = require('express');
-const multer = require('multer');
-const imageService = require('../schema/ImageService');
-
-module.exports = () => {
-    // https://github.com/expressjs/multer#memorystorage
-    // use memoryStorage to generate buffer for the file
-    const upload = multer({ storage: multer.memoryStorage() });
-    const router = express.Router();
-
-    router.get('/', (req, res) => {
-        res.render('upload');
-    });
-
-    router.post('/', upload.any(), (req, res) => {
-        const image = {
-            data: req.files[0].buffer,
-            originalName: req.files[0].originalname,
-            contentType: req.files[0].mimetype,
-        };
-        imageService.create(image,(err, docs) => {
-    		if (err) throw err;
-    	});
-    });
-
-    return router;
-};
+// // Created by Yuxi Luo, July 2018
+//
+// const express = require('express');
+// const multer = require('multer');
+// const imageService = require('../schema/ImageService');
+//
+// module.exports = () => {
+//     // https://github.com/expressjs/multer#memorystorage
+//     // use memoryStorage to generate buffer for the file
+//     const upload = multer({ storage: multer.memoryStorage() });
+//     const router = express.Router();
+//
+//     router.get('/', (req, res) => {
+//         res.render('upload');
+//     });
+//
+//     router.post('/', upload.any(), (req, res) => {
+//         const image = {
+//             data: req.files[0].buffer,
+//             originalName: req.files[0].originalname,
+//             contentType: req.files[0].mimetype,
+//         };
+//         imageService.create(image,(err, docs) => {
+//     		if (err) throw err;
+//     	});
+//     });
+//
+//     return router;
+// };
